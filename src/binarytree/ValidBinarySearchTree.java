89684bc5b941a58589ec58a269fc633b4233a499
Hanyu Wang <hanyuw.edu@gmail.com>
2018-11-24 22:16:26 -0800

    Update ValidBST

--- src/binarytree/ValidBinarySearchTree.java
+++ src/binarytree/ValidBinarySearchTree.java
@@ -2,18 +2,67 @@ package binarytree;
 
 public class ValidBinarySearchTree {
     /**
+     * Apply isNull information
+     * 11/24/2018
+     *
+     * @param root: The root of binary tree.
+     * @return: True if the binary tree is BST, or false
+     */
+    public boolean isValidBST(TreeNode root) {
+        return divideConquer(root).isValid;
+    }
+
+    private class Info {
+        boolean isValid;
+        int max, min;
+        boolean isNull;
+
+        private Info(boolean isValid, int max, int min, boolean isNull) {
+            this.isValid = isValid;
+            this.max = max;
+            this.min = min;
+            this.isNull = isNull;
+        }
+    }
+
+    private Info divideConquer(TreeNode root) {
+        if (root == null) {
+            return new Info(true, 0, 0, true);
+        }
+
+        Info left = divideConquer(root.left);
+        Info right = divideConquer(root.right);
+
+        boolean isValid = true;
+        int min = root.val, max = root.val;
+
+        if (!left.isNull) {
+            isValid = isValid && left.isValid && left.max < root.val;
+            min = left.min;
+        }
+
+        if (!right.isNull) {
+            isValid = isValid && right.isValid && right.min > root.val;
+            max = right.max;
+        }
+
+        return new Info(isValid, max, min, false);
+    }
+
+
+    /**
      * Use node instead of value to better keep track of null situation
      * 6/2/2018
      *
      * @param root: The root of binary tree.
      * @return: True if the binary tree is BST, or false
      */
-    private class Info {
+    private class Info2 {
         TreeNode min;
         TreeNode max;
         boolean isValid;
 
-        private Info() {
+        private Info2() {
             this.min = null;
             this.max = null;
             this.isValid = true;
@@ -24,15 +73,15 @@ public class ValidBinarySearchTree {
         return divideAndConquer(root).isValid;
     }
 
-    private Info divideAndConquer(TreeNode root) {
+    private Info2 divideAndConquer(TreeNode root) {
         if (root == null) {
-            return new Info();
+            return new Info2();
         }
 
-        Info left = divideAndConquer(root.left);
-        Info right = divideAndConquer(root.right);
+        Info2 left = divideAndConquer(root.left);
+        Info2 right = divideAndConquer(root.right);
 
-        Info current = new Info();
+        Info2 current = new Info2();
         if (right.max == null) {
             current.max = root;
         } else {
